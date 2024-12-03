96ffa900b0404952b5c814330d74378d0fbea72b
Elad <567255+eladgi@users.noreply.github.com>
2024-12-03 18:56:36 +0200

    שיפורים למשחק: הוספת צלילים ורקע

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ let score = 0;  // משתנה לניקוד
 
 function generateExercise() {
     // ייצור תרגיל אקראי
+    document.getElementById("userAnswer").value = ""
     const isAddition = Math.random() > 0.5; // חיבור או חיסור
-    
+
     let num1, num2, exercise;
-    
+
     if (isAddition) {
         // חיבור, סכום עד 100
         num1 = Math.floor(Math.random() * 101); // מספר בין 0 ל-100
@@ -32,17 +33,31 @@ function generateExercise() {
 // פונקציה לבדוק את התשובה
 function checkAnswer() {
     const userAnswer = parseInt(document.getElementById("userAnswer").value);
+    if (isNaN(userAnswer) || userAnswer < 0 || userAnswer > 100) {
+        document.getElementById("result").textContent = "אנא הכנס מספר בין 0 ל-100.";
+        document.getElementById("result").style.color = "darkred";
+        document.getElementById("userAnswer").value = ""
+        document.getElementById("userAnswer").focus();
+        return;
+    }
+    const successSound = document.getElementById("successSound");
+    const failureSound = document.getElementById("failureSound");
+
     if (userAnswer === correctAnswer) {
         score++;  // הגדלת הניקוד
         document.getElementById("result").textContent = "תשובה נכונה!";
         document.getElementById("result").style.color = "green";
+        successSound.play(); // נגינת צליל הצלחה
         setTimeout(() => {
             generateExercise(); // יצירת תרגיל חדש אחרי 1.5 שניות
         }, 1500);
     } else {
-        document.getElementById("result").textContent = "תשובה לא נכונה, נסה שוב.";
-        document.getElementById("result").style.color = "red";
+        document.getElementById("result").textContent = userAnswer + " היא לא התשובה הנכונה, נסה שוב.";
+        document.getElementById("result").style.color = "darkred";
+        failureSound.play(); // נגינת צליל כישלון
+        document.getElementById("userAnswer").value = ""
     }
+    document.getElementById("userAnswer").focus();
 }
 
 // יצירת תרגיל ראשון
